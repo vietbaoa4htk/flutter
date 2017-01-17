421eb601f053811877e5366905a2402d4ffe0729
Bao Nguyen <vietbaoa4htk@gmail.com>
2017-01-17 16:59:20 +0900

    create Pages controller explore home index profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+	root 'pages#index'
+
+  get 'pages/index'
+
+# bo doan page ra
+  get 'home' => 'pages#home'
+
+  get 'profile' => 'pages#profile'
+ 
+  get 'explore' => 'pages#explore'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
